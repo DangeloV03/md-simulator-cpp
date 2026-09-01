1730da2d5f2580b6e147268d4c062bee2a808d57
DangeloV03 <angelv4331@gmail.com>
2026-09-01 15:37:01 -0400

    Implement Vec3 operators and Forward Euler integrator loop

--- main.cpp
+++ main.cpp
@@ -9,4 +9,19 @@ int main() {
 
     std::cout << "Particle position: " << particle.position.x << ", " << particle.position.y << ", " << particle.position.z << std::endl;
 
+    
+    particle.position = Vec3{0.0, 0.0, 0.0};
+    particle.velocity = Vec3{0.0, 10.0, 0.0};
+    particle.acceleration = Vec3{0.0, -9.81, 0.0};
+    particle.mass = 1.0;
+    double dt = 0.01;
+    int total_steps = 100;
+
+    for (int step = 0; step < total_steps; step++) {
+        particle.position = particle.position + particle.velocity * dt;
+        particle.velocity = particle.velocity + particle.acceleration * dt;
+        std::cout << "Step " << step << ": " << particle.position.x << ", " << particle.position.y << ", " << particle.position.z << std::endl;
+
+
+    }
 }
--- vec3.h
+++ vec3.h
@@ -2,4 +2,12 @@
 
 struct Vec3 {
     double x, y, z;
+
+    Vec3 operator+(const Vec3& other) const {
+        return Vec3{x + other.x, y + other.y, z + other.z};
+    }
+
+    Vec3 operator*(const double& scalar) const {
+        return Vec3{x * scalar, y * scalar, z * scalar};
+    }
 };
